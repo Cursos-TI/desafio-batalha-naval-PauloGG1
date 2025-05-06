fb4207cc96997b4d1f1374cd009a40ab9803e932
PauloGG1 <pgaspar1612@gmail.com>
2025-05-06 11:39:11 +0000

    fiz os vetores unidimensionais que representam os navios com valores 3

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -8,5 +8,13 @@
 #define TAM_TABULEIRO 10 
 #define TAM_NAVIO 3
 
+int main() {
+int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO] = {0};
+
+// Vetores unidimensionais que representam os navios com valores 3
+
+int navioH[TAM_NAVIO] = {3, 3, 3}; // Navio da horizontal
+int navioV[TAM_NAVIO] = {3, 3, 3}; // Navio da vertical
+
 
 
